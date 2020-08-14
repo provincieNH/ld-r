616556c58afade9abb15dafc0eaff7c419099b46
Niels <niels.hoffmann@hotmail.com>
2020-08-14 16:15:33 +0200

    eerste updates pnh specifieke configuratie ld-r

--- components/Home.js
+++ components/Home.js
@@ -8,12 +8,13 @@ class Home extends React.Component {
                     <div className="ui row">
                         <div className="column">
                             <div className="ui segment content">
-                                <h2 className="ui header">Linked Data Reactor (LD-R)</h2>
+                                <h2 className="ui header">Datacatalogus - Provincie Noord-Holland Data Platform</h2>
                                 <p>
                                     <img className="ui left floated image animated zoomIn" src="/assets/img/ld-reactor.gif" alt="ld-reactor" />
-                                    Linked Data Reactor (LD-Reactor or LD-R) is a framework to develop <b>reactive</b> and <b>reusable</b> User Interface components for <b>Linked Data applications</b>. LD-Reactor utilizes Facebook's <a href="https://facebook.github.io/react/">ReactJS</a> components, <a href="https://facebook.github.io/flux">Flux</a> architecture and Yahoo!'s <a href="http://fluxible.io/">Fluxible</a> framework for isomorphic Web applications.
-                                    It also exploits <a href="http://semantic-ui.com/"> Semantic-UI </a> framework for flexible UI themes.
-                                    LD-Reactor aims to apply the idea of component-based application development into <a href="http://www.w3.org/RDF/">RDF</a> data model hence enhancing current user interfaces to view, browse and edit <a href="http://linkeddata.org/">Linked Data</a>.<br/> More information is available at <a href="http://ld-r.org">ld-r.org</a>.<br/><br/>
+                                    De datacatalogus is een viewer gebaseerd op het <a href="http://ld-r.org"> LD-R </a> framework. <br/>
+                                    Hier vind je alle datasets die beschikbaar zijn op het dataplatform. <br/>
+                                    Je kunt zoeken in de catalogus en de metadata van de verschillende datasets raadplegen.
+                                    
                                 </p>
                             </div>
                         </div>
---
+++ configs/server.pnh.js
@@ -0,0 +1,31 @@
+//important: first value in the array is considered as default value for the property
+//this file is visible to the server-side
+export default {
+    serverPort: [4000],
+    sparqlEndpoint: {
+        // config in generic
+        'generic': {
+            host: '10.0.0.127', port: 7200, path: '/repositories/ld-r-config', graphName: 'http://ld-r.org/users', endpointType: 'graphdb'
+        },
+        //DBPedia example dataset
+        'http://dbpedia.org/sparql': {
+            host: 'dbpedia.org', port: 80, path: '/sparql', graphName: 'default', endpointType: 'virtuoso'
+        },
+        //GraphDB triple store Datacatalogus
+        'datacatalogus': {
+            host: '10.0.0.127', port: 7200, path: '/repositories/datacatalogus', graphName: 'http://ld.noord-holland.nl/catalogus/', endpointType: 'graphdb'
+        },
+    },
+    dbpediaLookupService: [
+        { host: 'lookup.dbpedia.org' }
+    ],
+    dbpediaSpotlightService: [
+        { host: 'api.dbpedia-spotlight.org', port: 80, path: '/en/annotate' }
+    ],
+    //it is used only if you enabled recaptcha feature for user authentication
+    //get keys from https://www.google.com/recaptcha
+    googleRecaptchaService: {
+        siteKey: ['put your google recaptcha site key here...'],
+        secretKey: ['put your google recaptcha secret key here...']
+    }
+};
